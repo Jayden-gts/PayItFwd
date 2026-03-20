e7fe8dc6cf40864f356500125643e1df01335a43
Jayden Seto <setoja@sheridancollege.ca>
2026-03-19 20:00:41 -0400

    Initial Commit

---
+++ PayItFwd/PayItFwdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayItFwdApp.swift
+//  PayItFwd
+//
+//  Created by Jayden Seto on 2026-03-19.
+//
+
+import SwiftUI
+
+@main
+struct PayItFwdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PayItFwdTests/PayItFwdTests.swift
@@ -0,0 +1,17 @@
+//
+//  PayItFwdTests.swift
+//  PayItFwdTests
+//
+//  Created by Jayden Seto on 2026-03-19.
+//
+
+import Testing
+@testable import PayItFwd
+
+struct PayItFwdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
